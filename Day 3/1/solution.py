547424b0de6b10689400fc4db2b89503713b2897
Emil Nilsson <nilsson.emi@gmail.com>
2019-12-04 07:33:38 +0100

    added some docs

--- Day 3/1/solution.py
+++ Day 3/1/solution.py
@@ -4,14 +4,14 @@ with open("3.in", "r") as f:
         loc = [0, 0]
 
         for move in f.readline().split(","):
-            change = {"L": (0, -1), "R": (0, 1), "U": (1, 1), "D": (1, -1)}[move[0]]
-            for _ in range(int(move[1:])):
-                loc[change[0]] += change[1]
-                yield tuple(loc)
+            change = {"L": (0, -1), "R": (0, 1), "U": (1, 1), "D": (1, -1)}[move[0]] # Look at the first char and map that to a change in the tuple 
+            for _ in range(int(move[1:])): # Iterate over the number of steps to be taken
+                loc[change[0]] += change[1] # Add the change to the location array
+                yield tuple(loc) # Return the current value to be added to the visited array
 
     visited = set(followWire()) #Array of tuples with all visited cords
 
-    intersections = []
+    intersections = [] # Array to hold all intersections
     for loc in followWire():
         if loc in visited:
             intersections.append(abs(loc[0]) + abs(loc[1]))
--- Day 3/2/solution.py
+++ Day 3/2/solution.py
@@ -4,15 +4,15 @@ with open("3.in", "r") as f:
         steps = 0
 
         for move in f.readline().split(","):
-            change = {"L": (0, -1), "R": (0, 1), "U": (1, 1), "D": (1, -1)}[move[0]]
-            for _ in range(int(move[1:])):
-                loc[change[0]] += change[1]
-                steps = steps + 1
-                yield tuple(loc), steps
+            change = {"L": (0, -1), "R": (0, 1), "U": (1, 1), "D": (1, -1)}[move[0]] # Look at the first char and map that to a change in the tuple 
+            for _ in range(int(move[1:])): # Iterate over the number of steps to be taken
+                loc[change[0]] += change[1] # Add the change to the location array
+                steps = steps + 1 # Add a step
+                yield tuple(loc), steps # Return the current value and steps to be added to the visited array
 
-    visited = {}
+    visited = {} #Array of tuples with all visited cords
 
-    intersections = []
+    intersections = [] # Array to hold all intersections
     for loc, steps in followWire():
         if loc not in visited:
             visited[loc] = steps
